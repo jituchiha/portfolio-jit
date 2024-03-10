ab093e246784dfe2419095c4dc79921aa0d256bc
JITESH BHANDARI <jitbhand@iu.edu>
2024-03-10 16:08:06 -0400

    added svgs in images folder

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
-import GithubIcon from "../../../portfolio/public/github-icon.svg";
-import LinkedinIcon from "../../../portfolio/public/linkedin-icon.svg";
+import GithubIcon from "../../public/images/github-icon.svg";
+import LinkedinIcon from "../../public/images/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
 
