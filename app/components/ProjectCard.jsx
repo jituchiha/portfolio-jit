7fd6059dfa82ea92406e68f449450d0b5a8ba4bd
JITESH BHANDARI <jitbhand@iu.edu>
2024-03-10 17:08:12 -0400

    custom svgs

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+//import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+import CodeBrack from "../../public/images/CodeBrac.svg"
+import Eye from "../../public/images/Eye.svg"
 import Link from "next/link";
-import { SolidIconName, OutlineIconName } from '@heroicons/react/24/solid';
+//import { SolidIconName, OutlineIconName } from '@heroicons/react/24/outline';
 
 
 const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
@@ -16,13 +18,13 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
             href={gitUrl}
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
-            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+            <CodeBrack className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link>
           <Link
             href={previewUrl}
             className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
-            <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+            <Eye className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link>
         </div>
       </div>
